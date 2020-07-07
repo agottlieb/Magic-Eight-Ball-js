afe1e0a493f118d31c246fb95f131edac9d97001
agottlieb <allison.m.gottlieb@gmail.com>
2020-07-07 13:20:53 -0400

    updated picture link

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ magic8ball.listOfAnswers = ["Seems promising.", "Signs point to no.", "Definatel
   $("#answer").hide ();
  magic8ball.askQuestion =function (question) {
    $("#answer").fadeIn(4000);
-      $("#8ball").attr("src", "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/magic8ballAnswer.png");
+      $("#8ball").attr("src", "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/answerside.png");
    var randomNumber = Math.random();
    var randomNumberForListOfAnswers = randomNumber * this.listOfAnswers.length;
    var randomIndex = Math.floor(randomNumberForListOfAnswers);
