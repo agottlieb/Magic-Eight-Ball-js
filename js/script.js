372a28abaf494008e82639fa8d393eff2fcab1c3
agottlieb <allison.m.gottlieb@gmail.com>
2020-07-08 13:27:09 -0400

    removed extra code

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
 var magic8ball = {};
 magic8ball.listOfAnswers = ["Seems promising.", "Signs point to no.", "Definately yes.", "In your dreams.", "Indubitably."];
- $("#answer").hide ();
+$("#answer").hide ();
 
- $("#answer").hide ();
+
  magic8ball.askQuestion =function (question) {
    $("#answer").fadeIn(4000);
    $("#8ball").attr("src", "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/answerside.png");
